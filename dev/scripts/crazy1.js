ccf7957b0e87ec2c0a566f43829372a2715369ec
Lukas.Zech <lukas.zech@check24.de>
2013-12-16 15:25:41 +0100

    tried something on Crazy1

--- dev/scripts/crazy1.js
+++ dev/scripts/crazy1.js
@@ -1,15 +1,34 @@
-define(["knockout"], function (ko) {
+define(["knockout", "dataService"], function (ko, DataService, undefined) {
 
-  var Crazy1ViewModel;
+    var crazy1ViewModel,
+        dataService;
 
-  Crazy1ViewModel = function () {
-    if (!(this instanceof Crazy1ViewModel)) {
-        return new Crazy1ViewModel();
-    }
+    crazy1ViewModel = function () {
+        if (!(this instanceof crazy1ViewModel)) {
+            return new crazy1ViewModel();
+        }
 
-    this.message = ko.observable("This message comes from Crazy1ViewModel.");
-  };
+        var self = this;
+        
+        self.availableDoughs = ko.observableArray();
 
-  return Crazy1ViewModel;
+        self.selectedDough = ko.computed(function () {
+            return ko.utils.arrayFirst(self.availableDoughs(), function (item) {
+                return item.id == id; //selected id will be a string
+            });
+        });
+        
+        self.showSelectedDough = ko.computed(function () {
+            debugger;
+            return self.selectedDough() !== undefined;
+        });
+
+        dataService = new DataService();
+        dataService.getDoughs().done(function (result) {
+            self.availableDoughs(result);
+        });
+    };
+
+    return crazy1ViewModel;
 
 });
