0d3f232ebdac35f9487fe3be5308d1eb9eec6427
Andreas Helmberger <andreas.helmberger@check24.de>
2014-02-25 18:10:24 +0100

    Move pizza loading logic from "menu" to "menuAllaCarte"

--- test/unit/allaCarteMenuSpec.js
+++ test/unit/allaCarteMenuSpec.js
@@ -1,4 +1,4 @@
-define(["allaCarteMenu", "basket"], function (allaCarteMenu, basket) {
+define(["squire", "q", "allaCarteMenu", "basket"], function (Squire, Q, allaCarteMenu, basket) {
     describe("When the menu is empty", function () {
         it ("should return isEmpty true when the menu contains any pizza", function () {
             expect(allaCarteMenu.isEmpty()).toEqual(true);
@@ -7,7 +7,7 @@ define(["allaCarteMenu", "basket"], function (allaCarteMenu, basket) {
 
     describe("When the menu contains pizzas", function () {
         beforeEach(function () {
-            allaCarteMenu.addPizza({name: "Salami" });
+            allaCarteMenu.pizzas.push({ name: "Salami" });
         });
 
         it ("should return isEmpty false", function () {
@@ -16,11 +16,37 @@ define(["allaCarteMenu", "basket"], function (allaCarteMenu, basket) {
 
         describe("When a pizza has been selected", function () {
             beforeEach(function () {
-                allaCarteMenu.selectPizza ({name: "Salami"});
+                allaCarteMenu.selectPizza ({ name: "Salami" });
             });
 
             it ("should add the selected pizza to the basket", function () {
                 expect(basket.hasPizzas()).toEqual(true);
+            });
+        });
+    });
+
+    describe("On allaCarteMenu", function () {
+        describe("When the menu is created", function () {
+            var dataServiceMock,
+                squire,
+                async = new AsyncSpec(this);
+           
+            async.beforeEach(function (done) {
+                dataServiceMock = jasmine.createSpyObj("dataService", ["getPizzas"]);
+                dataServiceMock.getPizzas.andReturn(Q.when([{}]));
+
+                squire = new Squire();
+                squire.mock("dataService", function () {
+                    return dataServiceMock;
+                });
+                
+                squire.require(["allaCarteMenu"], function () {
+                    done();
+                });
+            });
+
+            it ("should call the express server", function () {
+                expect(dataServiceMock.getPizzas).toHaveBeenCalled();
             });
         });
     });
--- test/unit/menuSpec.js
+++ test/unit/menuSpec.js
@@ -1,28 +1,3 @@
-define(["squire", "q"], function (Squire, Q) {
-    describe("On menu alla Carte", function (){
-        describe("When the menu is created", function () {
-            var menuViewModel,
-                dataServiceMock,
-                injector,
-                async = new AsyncSpec(this);
-           
-            async.beforeEach(function (done){
-                dataServiceMock = jasmine.createSpyObj("dataService", ["getPizzas"]);
-                dataServiceMock.getPizzas.andReturn(Q.when([{}]));
-
-                injector = new Squire();
-                injector.mock("dataService", function () {
-                    return dataServiceMock;
-                });
-                
-                injector.require(["menu"], function (menuViewModel){
-                    done();
-                });
-            });
-
-            it ("should call the express server", function () {
-                expect(dataServiceMock.getPizzas).toHaveBeenCalled();
-            });
-        });
-    });
+define([], function () {
+  // Tests
 });
