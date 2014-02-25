0b357b4fd443e5ab4d946c2631e570a982d51079
Andreas Helmberger <andreas.helmberger@check24.de>
2014-02-25 18:32:05 +0100

    Add test for menu view model

--- test/unit/menuSpec.js
+++ test/unit/menuSpec.js
@@ -1,3 +1,37 @@
-define([], function () {
-  // Tests
+define(["squire"], function (Squire) {
+
+  describe("Menu", function () {
+
+    describe("When initialized", function () {
+
+      describe("when the basket is in crazy mode", function () {
+
+        var async = new AsyncSpec(this),
+            resetSpy = jasmine.createSpy("resetSpy"),
+            basketMock = {
+              isCrazy: function () {
+                return true;
+              },
+              reset: resetSpy
+            };
+
+        async.beforeEach(function (done) {
+          new Squire()
+            .mock("basket", basketMock)
+            .require(["menu"], function (menu) {
+              menu.initialize();
+              done();
+            });
+        });
+
+        it ("should reset the basket", function () {
+          expect(resetSpy).toHaveBeenCalled();
+        });
+
+      });
+
+    });
+
+  });
+
 });
